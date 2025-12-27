67df041b5bdadf910f559c29ae39785ea2cd34f2
zinh8803 <ngoquocvinh2003@gmail.com>
2025-12-27 14:00:50 +0700

    save

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 import store from "../store";
 import { setUser, logout } from "../store/userSlice";
-const API_URL = import.meta.env.VITE_API_URL;
+// const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = "/api";
 const api = axios.create({
     baseURL: API_URL,
     timeout: 60000,
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
+dotenv.config()
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
   ],
   server: {
     host: '0.0.0.0',
-    port: 5173
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: process.env.VITE_API_URL,
+        changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   }
 })
